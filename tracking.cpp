c0775fa45d8dcb95f0779f66081d17043c026680
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-01-18 11:30:51 +0100

    Some refactoring into functions

--- tracking.cpp
+++ tracking.cpp
@@ -17,14 +17,60 @@ using namespace std;
 
 #define HAAR_DATA_DIR "/usr/share/opencv/haarcascades/"
 
+bool track_frame(const string& trackerType, Rect2d& bbox, Tracker* tracker, Mat& frame)
+{
+    // Start timer
+    double timer = (double)getTickCount();
+    // Update the tracking result
+    bool ok = tracker->update(frame, bbox);
+    // Calculate Frames per second (FPS)
+    float fps = getTickFrequency() / ((double)getTickCount() - timer);
+    if (ok) {
+        // Tracking success : Draw the tracked object
+        rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    } else {
+        putText(frame, "Tracking failure detected", Point(100,80),
+                FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
+    }
+
+    // Display tracker type and FPS on frame
+    putText(frame, trackerType + " Tracker", Point(100,20),
+            FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
+    putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50),
+            FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+
+    return ok;
+}
+
+bool detect_face(Tracker* tracker, Mat& frame, Rect2d& bbox)
+{
+    // Detect Face using Haar Cascade
+    CascadeClassifier face_cascade;
+    face_cascade.load(HAAR_DATA_DIR "haarcascade_frontalface_alt2.xml");
+    std::vector<Rect> f;
+    face_cascade.detectMultiScale(frame, f, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE);
+    if (f.size() == 0)
+        return false;
+
+    // Define initial bounding box for the face detected in first frame i.e.
+    // f[0]. Convert Rect co ordinates to Rect2D.
+    Rect2d bbox_tmp(f[0].x, f[0].y, f[0].width, f[0].height);
+    bbox = bbox_tmp;
+
+    // Display bounding box.
+    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+    imshow("Tracking", frame);
+
+    tracker->init(frame, bbox);
+
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     // List of tracker types in OpenCV 3.2
     // NOTE : GOTURN implementation is buggy and does not work.
     string trackerTypes[6] = {"BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN"};
-    // vector <string> trackerTypes(types, std::end(types));
-
     // Create a tracker and select type by choosing indicies
     string trackerType = trackerTypes[4];
 
@@ -51,7 +97,8 @@ int main(int argc, char **argv)
     }
     #endif
     // Read video
-    VideoCapture video("mcem0_head.mpg");
+    //VideoCapture video("mcem0_head.mpg");
+    VideoCapture video(0);
 
     // Exit if video is not opened
     if(!video.isOpened())
@@ -60,55 +107,16 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // Read first frame
     Mat frame;
-    bool ok = video.read(frame);
-
-    // Detect Face using Haar Cascade
-    CascadeClassifier face_cascade;
-    face_cascade.load(HAAR_DATA_DIR "haarcascade_frontalface_alt2.xml");
-    std::vector<Rect> f;
-    face_cascade.detectMultiScale(frame, f, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE);
-
-    // Define initial bounding box for the face detected in first frame i.e.
-    // f[0]. Convert Rect co ordinates to Rect2D.
-    Rect2d bbox(f[0].x,f[0].y,f[0].width,f[0].height);
-
-    // Uncomment the line below to select a a bounding box manually
-    // bbox = selectROI(frame, false);
-
-    // Display bounding box.
-    rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-    imshow("Tracking", frame);
-
-    tracker->init(frame, bbox);
-
+    Rect2d bbox;
+    bool tracking = false;
     while(video.read(frame))
     {
-        // Start timer
-        double timer = (double)getTickCount();
-        // Update the tracking result
-        bool ok = tracker->update(frame, bbox);
-        // Calculate Frames per second (FPS)
-        float fps = getTickFrequency() / ((double)getTickCount() - timer);
-        if (ok)
-        {
-            // Tracking success : Draw the tracked object
-            rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
-        }
-        else
-        {
-            // Tracking failure detected.
-            putText(frame, "Tracking failure detected", Point(100,80),
-                    FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
-        }
+        if (tracking)
+            tracking = track_frame(trackerType, bbox, tracker, frame);
 
-        // Display tracker type on frame
-        putText(frame, trackerType + " Tracker", Point(100,20),
-                FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50),2);
-        // Display FPS on frame
-        putText(frame, "FPS : " + SSTR(int(fps)), Point(100,50),
-                FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50,170,50), 2);
+        if (!tracking)
+            tracking = detect_face(tracker, frame, bbox);
 
         // Display frame.
         imshow("Tracking", frame);
@@ -116,8 +124,6 @@ int main(int argc, char **argv)
         // Exit if ESC pressed.
         int k = waitKey(1);
         if(k == 27)
-        {
             break;
-        }
     }
 }
